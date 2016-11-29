3279c2529140507af1686878c9b048959ded7d5d
rhetty <hjw.rhett@qq.com>
2016-11-30 00:48:44 +0800

    show courses & notifications & assignments

--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -1,19 +1,26 @@
-var user = require('../../utils/user.js')
+import User from '../../utils/user'
 
 Page({
     data:{
         hint: ''
     },
     formSubmit:function(e) {
+        wx.showToast({
+            title: '请稍等',
+            icon: 'loading',
+            duration: 10000
+        })
         var input = e.detail.value
-        var bindSuccess = user.bindAccount(input.username_input, input.password_input)
-        if (bindSuccess) {
-            this.setData({hint:''})
-            wx.redirectTo({
-              url: '../timeline/timeline',
-            })
-        } else {
-            this.setData({hint:'绑定失败'})
-        }
+        let that = this
+        User.sharedInstance().bindAccount(input.username_input, input.password_input, function(success){
+            wx.hideToast()
+            if(success) {
+                wx.redirectTo({
+                url: '../timeline/timeline',
+                })
+            } else {
+                that.setData({hint:'绑定失败'})
+            }
+        })
     }
 })
